ad8af9d508ac2291b49091735d3e0a0caf1bf02b
Eugene N <kan1fol@ya.ru>
2023-07-29 13:16:15 +0200

    Code: added text representation for category

--- src/model/ProductCategory.java
+++ src/model/ProductCategory.java
@@ -1,10 +1,20 @@
 package model;
 
 public enum ProductCategory {
-    toys,
-    carParts,
-    drinks,
-    foods,
-    officeSupplies,
-    other
+    toys("Toys"),
+    carParts("Car parts"),
+    drinks("Drinks"),
+    foods("Food"),
+    officeSupplies("Office supplies"),
+    other("Other");
+
+    public final String text;
+    ProductCategory(final String text){
+        this.text = text;
+    }
+
+    @Override
+    public String toString(){
+        return this.text;
+    }    
 }
